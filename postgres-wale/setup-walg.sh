d5a25ef21056a24cc7ca568f425f2a9dc30ec234
Semion Sidorenko <semion.sidorenko@gmail.com>
2020-01-08 16:26:10 +0100

    postgres-walg WIP

--- postgres-wale/setup-walg.sh
+++ postgres-wale/setup-walg.sh
@@ -8,29 +8,30 @@
 # Assumption: the group is trusted to read secret information
 umask u=rwx,g=rx,o=
 
-# Create the archival wal-e environment
-mkdir -p /etc/wal-e.d/env-archive
-echo "$AWS_SECRET_ACCESS_KEY" > /etc/wal-e.d/env-archive/AWS_SECRET_ACCESS_KEY
-echo "$AWS_ACCESS_KEY_ID" > /etc/wal-e.d/env-archive/AWS_ACCESS_KEY_ID
-echo "$ARCHIVE_S3_PREFIX" > /etc/wal-e.d/env-archive/WALE_S3_PREFIX
+# Create the archival wal-g environment
+mkdir -p /etc/wal-g.d/env-archive
+echo "$AWS_SECRET_ACCESS_KEY" > /etc/wal-g.d/env-archive/AWS_SECRET_ACCESS_KEY
+echo "$AWS_ACCESS_KEY_ID" > /etc/wal-g.d/env-archive/AWS_ACCESS_KEY_ID
+echo "$ARCHIVE_S3_PREFIX" > /etc/wal-g.d/env-archive/WALG_S3_PREFIX
+echo "$AWS_ENDPOINT" > /etc/wal-g.d/env-archive/AWS_ENDPOINT
 
-# Setup the archive wal-e configuration
+# Setup the archive wal-g configuration
 echo "wal_level = archive" >> /var/lib/postgresql/data/postgresql.conf
 echo "archive_mode = on" >> /var/lib/postgresql/data/postgresql.conf
-echo "archive_command = 'envdir /etc/wal-e.d/env-archive /usr/local/bin/wal-e wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
+echo "archive_command = 'envdir /etc/wal-g.d/env-archive /usr/local/bin/wal-g wal-push %p'" >> /var/lib/postgresql/data/postgresql.conf
 echo "archive_timeout = 60" >> /var/lib/postgresql/data/postgresql.conf
 
 # Restore from the latest backup if RESTORE_S3_PREFIX is set
 if [ "$RESTORE_S3_PREFIX" != "" ]; then
-  mkdir -p /etc/wal-e.d/env-restore
-  cp /etc/wal-e.d/env-archive/* /etc/wal-e.d/env-restore/
-  echo "$RESTORE_S3_PREFIX" > /etc/wal-e.d/env-restore/WALE_S3_PREFIX
+  mkdir -p /etc/wal-g.d/env-restore
+  cp /etc/wal-g.d/env-archive/* /etc/wal-g.d/env-restore/
+  echo "$RESTORE_S3_PREFIX" > /etc/wal-g.d/env-restore/WALG_S3_PREFIX
 
   if [ ! -f /var/lib/postgresql/data/recovery.done ]; then
     pg_ctl -D "$PGDATA" -m immediate stop
 
-    /usr/bin/envdir /etc/wal-e.d/env-restore /usr/local/bin/wal-e backup-fetch --blind-restore /var/lib/postgresql/data/ LATEST
-    echo "restore_command = '/usr/bin/envdir /etc/wal-e.d/env-restore /usr/local/bin/wal-e wal-fetch \"%f\" \"%p\"'" >> /var/lib/postgresql/data/recovery.conf
+    /usr/bin/envdir /etc/wal-g.d/env-restore /usr/local/bin/wal-g backup-fetch --blind-restore /var/lib/postgresql/data/ LATEST
+    echo "restore_command = '/usr/bin/envdir /etc/wal-g.d/env-restore /usr/local/bin/wal-g wal-fetch \"%f\" \"%p\"'" >> /var/lib/postgresql/data/recovery.conf
     chown postgres:postgres /var/lib/postgresql/data/recovery.conf
 
     pg_ctl -D "$PGDATA" -o "-c listen_addresses=''" start -t 600
